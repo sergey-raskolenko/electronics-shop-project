272cda679be44798cff8ee8cd2be16e6caed3892
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-06-10 14:12:02 +0300

    Add fixture and comments

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,21 +1,34 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
+import pytest
+
+@pytest.fixture
+def item():
+	item = Item("Смартфон", 10000, 20)
+	return item
 
 
-def test__init__():
-	item1 = Item("Смартфон", 10000, 20)
-	assert item1.name == "Смартфон"
-	assert item1.price == 10000
-	assert item1.quantity == 20
+def test__init__(item):
+	"""
+	Проверка принадлежности экземляра класса к классу
+	"""
+	assert isinstance(item, Item)
+	assert item.name == "Смартфон"
+	assert item.price == 10000
+	assert item.quantity == 20
 
 
-def test_calculate_total_price():
-	item1 = Item("Смартфон", 10000, 20)
-	assert item1.calculate_total_price() == 200_000
+def test_calculate_total_price(item):
+	"""
+	Проверка подсчета общей стоимости продукта
+	"""
+	assert item.calculate_total_price() == 200_000
 
 
-def test_apply_discount():
-	item1 = Item("Смартфон", 10000, 20)
+def test_apply_discount(item):
+	"""
+	Проверка применнеия скидки к определенному товару
+	"""
 	Item.pay_rate = 0.8
-	item1.apply_discount()
-	assert item1.price == 8000
+	item.apply_discount()
+	assert item.price == 8000
