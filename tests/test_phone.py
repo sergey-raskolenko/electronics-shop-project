54566b42d0e7a3ec130c0f7187bd254d335b8435
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-06-22 23:30:18 +0300

    Added test for __add__

--- tests/test_phone.py
+++ tests/test_phone.py
@@ -2,11 +2,6 @@ from src.item import Item
 from src.phone import Phone
 import pytest
 
-@pytest.fixture()
-def phone():
-    phone = Phone("iPhone 14", 120_000, 5, 2)
-    return phone
-
 def test__init__(phone):
     assert phone.name == "iPhone 14"
     assert phone.price == 120_000
@@ -22,4 +17,8 @@ def test__repr_and__str__(phone):
     assert repr(phone) == "Phone('iPhone 14', 120000, 5, 2)"
     assert str(phone) == 'iPhone 14'
 
-
+def test__add__(item, phone, phone2):
+	assert phone + phone2 == 15
+	assert item + phone2 == 30
+	with pytest.raises(ValueError):
+		phone + 1
