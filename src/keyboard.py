3cc4387c09a550506e50da6e6dd222adedf36420
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-06-25 20:29:22 +0300

    Created class Keyboard

---
+++ src/keyboard.py
@@ -0,0 +1,30 @@
+from src.item import Item
+
+class KeyboardLanguages:
+    """
+    Миксик-класс для смены раскладки клавиатуры
+    """
+    def __init__(self, name: str, price: float, quantity: int):
+        self.__language = "EN"
+        super().__init__(name, price, quantity)
+
+    @property
+    def language(self):
+        """Геттер для расклдаки клавиатуры"""
+        return self.__language
+
+    def change_lang(self):
+        """
+        Изменяет раскладку клавиатуры
+        """
+        if self.__language is "EN":
+            self.__language = "RU"
+        else:
+            self.__language = "EN"
+
+class KeyBoard(KeyboardLanguages, Item):
+    """
+    Класс для создания экземпляра 'Клавиатура'
+    """
+    def __init__(self, name: str, price: float, quantity: int):
+        super().__init__(name, price, quantity)
