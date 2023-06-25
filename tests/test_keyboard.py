5be11e835103aaf07b5a88767f743a309346f109
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-06-25 20:29:17 +0300

    Created tests for KeyBoard object

---
+++ tests/test_keyboard.py
@@ -0,0 +1,20 @@
+from src.keyboard import KeyBoard, KeyboardLanguages
+from src.item import Item
+import pytest
+
+def test__init__():
+    kb = KeyBoard('Dark Project KD87A', 9600, 5)
+    assert isinstance(kb, KeyBoard)
+    assert isinstance(kb, KeyboardLanguages)
+    assert isinstance(kb, Item)
+    assert kb.name == 'Dark Project KD87A'
+    assert kb.price == 9600
+    assert kb.quantity == 5
+    assert kb.language == "EN"
+    kb.change_lang()
+    assert kb.language == "RU"
+    kb.change_lang()
+    kb.change_lang()
+    assert kb.language == "RU"
+    with pytest.raises(AttributeError):
+        kb.language = "CN"
